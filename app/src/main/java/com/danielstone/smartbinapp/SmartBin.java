eec68ad13e66d97cdcb3cedc184e75a6d9a948be
Daniel S <danielstone@Daniels-Mac-mini.local>
2015-12-21 10:13:36 +0000

    Initial commit

---
+++ app/src/main/java/com/danielstone/smartbinapp/SmartBin.java
@@ -0,0 +1,23 @@
+package com.danielstone.smartbinapp;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+
+public class SmartBin extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Enable Local Datastore.
+        Parse.ena1bleLocalDatastore(this);
+
+        // Add your initialization code here
+        Parse.initialize(this);
+
+        ParseACL defaultACL = new ParseACL();
+        ParseACL.setDefaultACL(defaultACL, true);
+    }
+}
